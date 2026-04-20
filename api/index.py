8120241276d0a1b33548a3fe0dc1a09cd5a306c7
Ahmet Gul <ahmetgul@gmail.com>
2026-04-19 22:14:06 -0400

    Engine: Use mobile clients to bypass YouTube bot detection

--- api/index.py
+++ api/index.py
@@ -50,7 +50,14 @@ def process_request(url, req_format, req_quality, use_proxy=True):
         'quiet': True,
         'format': format_str,
         'skip_download': True,
-        'nocheckcertificate': True
+        'nocheckcertificate': True,
+        'extractor_args': {
+            'youtube': {
+                'player_client': ['ios', 'android'],
+                'player_skip': ['webpage', 'configs']
+            }
+        },
+        'user_agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1'
     }
     
     if use_proxy:
